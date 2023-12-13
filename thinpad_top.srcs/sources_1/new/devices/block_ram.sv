78087fc180d01603aba75ef2fe4e4dc4a2b81c26
yuezq21 <yuezq21@mails.tsinghua.edu.cn>
2023-12-13 17:17:52 +0800

    attempt to fix wns failing 2

--- thinpad_top.srcs/sources_1/new/devices/block_ram.sv
+++ thinpad_top.srcs/sources_1/new/devices/block_ram.sv
@@ -33,125 +33,6 @@ module display_controller #(
 );
 //   // TODO: Implement this module, just instantiate the 
 //   // BRAM IP core here and do necessary conversions.
-
-//   // One may add VGA controller here for your convenience.
-//   parameter HMAX = 1040;
-//   parameter VMAX = 666;
-//   parameter HSIZE = 800;
-//   parameter VSIZE = 600;
-//   parameter VGA_SCALE = 3;  // 8 times compression
-
-//   parameter [ADDR_WIDTH-1:0] SWITCH_VRAM_ADDR = 32'h300F_0000;
-
-
-//   logic [BRAM_ADDR_WIDTH-1:0] write_addr;
-//   logic [1:0][BRAM_DATA_WIDTH-1:0] write_data;
-//   logic [BRAM_ADDR_WIDTH-1:0] read_addr;
-//   logic [1:0][BRAM_DATA_WIDTH-1:0] read_data;
-
-//   logic [1:0][BRAM_DATA_WIDTH/8-1:0] vram_we;
-
-//   logic cur_vram;
-
-//   // conversion to read address of vram.
-//   always_comb begin
-//     read_addr = (vga_hdata >> VGA_SCALE) + (vga_vdata >> VGA_SCALE) * (HSIZE >> VGA_SCALE) ;
-//     pixel = read_data[1 - cur_vram];
-//   end
-
-//   vga_mem vram0 (
-//       .clka (clk_i),
-//       .clkb (vga_clk),
-//       .wea  (vram_we[0]),
-//       .addra(write_addr),
-//       .dina (write_data[0]),
-
-//       .addrb(read_addr),
-//       .doutb(read_data[0])
-//   );
-
-//   vga_mem vram1 (
-//       .clka (clk_i),
-//       .clkb (vga_clk),
-//       .wea  (vram_we[1]),
-//       .addra(write_addr),
-//       .dina (write_data[1]),
-
-//       .addrb(read_addr),
-//       .doutb(read_data[1])
-
-//   );
-
-//   typedef enum reg [4:0] {
-//     WAIT,
-//     WRITE_BLOCKS,
-//     SWITCH_VRAM
-//   } state_t;
-
-//   state_t state;
-// always_comb begin
-//   case(state)
-//   WAIT: begin
-//     if(wb_cyc_i && wb_stb_i) begin
-//       if(wb_we_i) begin
-        
-//       end
-//     end
-//   end
-//   endcase
-// end
-//   always_ff @(posedge clk_i or posedge rst_i) begin
-//     if (rst_i) begin
-//       write_addr <= 0;
-//       write_data <= 0;
-//       vram_we    <= 0;
-//       cur_vram   <= 0;
-
-//       wb_ack_o <= 0;
-//       wb_dat_o <= 0;
-//       state <= WAIT;
-//     end else begin
-//       case (state)
-//         WAIT: begin
-//           wb_ack_o <= 0;
-//           if (wb_cyc_i && wb_stb_i) begin
-//             if (wb_we_i) begin
-//               if (wb_adr_i == SWITCH_VRAM_ADDR) begin
-//                 state <= SWITCH_VRAM;
-//               end else begin
-//                 state <= WRITE_BLOCKS;
-//                 write_addr[cur_vram] <= wb_adr_i[BRAM_ADDR_WIDTH-1:0];
-//                 vram_we[cur_vram] <= 1;
-//                 case (wb_sel_i)
-//                   4'b0001: write_data[cur_vram] <= wb_dat_i[7:0];
-//                   4'b0010: write_data[cur_vram] <= wb_dat_i[15:8];
-//                   4'b0100: write_data[cur_vram] <= wb_dat_i[23:16];
-//                   4'b1000: write_data[cur_vram] <= wb_dat_i[31:24];
-//                   default: begin
-//                   end
-//                 endcase
-//                 wb_ack_o <= 1;
-//               end
-//             end
-//           end
-//         end
-//         WRITE_BLOCKS: begin
-//           state <= WAIT;
-//           wb_ack_o <= 0;
-//         end
-//         SWITCH_VRAM: begin
-//           if (vga_vdata == VMAX - 1 && vga_hdata > HMAX - 15) begin
-//             wb_ack_o <= 1;
-//             cur_vram <= wb_dat_i[0];
-//             state <= WAIT;
-//           end else begin
-//             wb_ack_o <= 0;
-//           end
-//         end
-//       endcase
-//     end
-//   end
-
   parameter HMAX = 1040;
   parameter VMAX = 666;
   parameter HSIZE = 800;
@@ -186,7 +67,14 @@ module display_controller #(
       .addrb(read_addr_ram),
       .doutb(read_data[1])
   );
-
+  reg vga_can_switch;
+  always_ff @(posedge vga_clk) begin
+    if(rst_i ) begin
+      vga_can_switch <= 1'b0;
+    end else begin
+      vga_can_switch <= vga_vdata == VMAX - 1 && vga_hdata > HMAX - 15;
+    end
+  end
   always_comb begin
     vram_we = 4'b0;
     write_addr = 13'b0;
@@ -201,7 +89,7 @@ module display_controller #(
         write_data = wb_dat_i;
         wb_ack_o = 1'b1;
         end 
-        if(wb_adr_i == 32'h300F_0000 && vga_vdata == VMAX - 1 && vga_hdata > HMAX - 15) begin
+        if(wb_adr_i == 32'h300F_0000 && vga_can_switch) begin
           wb_ack_o = 1'b1;
         end
       end else begin
@@ -218,7 +106,7 @@ module display_controller #(
     if(rst_i) begin
       cur_vram_exposed <= 1'b0;
     end else begin
-      if(wb_adr_i == 32'h300F_0000 && vga_vdata == VMAX - 1 && vga_hdata > HMAX - 15) begin
+      if(wb_adr_i == 32'h300F_0000 && vga_can_switch) begin
         cur_vram_exposed <= wb_dat_i[0];
       end
     end
